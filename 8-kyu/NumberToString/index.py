eec223cfa88a5f71591508927015f8afe84c3f70
Txell Amigó <txellmgsol@gmail.com>
2023-01-28 18:28:47 +0100

    Number to String Kata

---
+++ 8-kyu/NumberToString/index.py
@@ -0,0 +1,30 @@
+'''
+Title:
+    Convert a Number to a String!
+DESCRIPTION:
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+
+*******************************************************************************
+Kata link:
+    https://www.codewars.com/kata/5265326f5fda8eb1160004c8/python
+
+Discuss link:
+    https://www.codewars.com/kata/5265326f5fda8eb1160004c8/discuss/python
+
+Solutions link:
+    https://www.codewars.com/kata/5265326f5fda8eb1160004c8/solutions/python    
+
+********************************************************************************
+'''
+
+
+# My Solution
+def number_to_string(num):
+    return str(num)
